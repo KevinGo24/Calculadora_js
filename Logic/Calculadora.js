0b12a5678d62d47268597f44fa4cfc49f34f8a3f
kevin_casa <kevinG>
2026-02-12 12:55:26 -0500

    se mejoro interfas y funciones

--- Logic/Calculadora.js
+++ Logic/Calculadora.js
@@ -1,8 +1,23 @@
 const Numero_1 = document.getElementById("Numero_A");
 const Numero_2 = document.getElementById("Numero_b");
 const resultado = document.getElementById("Mostrar");
-function Evento() {
+function Resta() {
     let a = parseFloat(Numero_1.value)
     let b = parseFloat(Numero_2.value)
     resultado.innerText = a - b
+}
+function Suma() {
+    let a = parseFloat(Numero_1.value)
+    let b = parseFloat(Numero_2.value)
+    resultado.innerText = a + b
+}
+function Multiplicar() {
+    let a = parseFloat(Numero_1.value)
+    let b = parseFloat(Numero_2.value)
+    resultado.innerText = a * b
+}
+function Dividir() {
+    let a = parseFloat(Numero_1.value)
+    let b = parseFloat(Numero_2.value)
+    resultado.innerText = a / b
 }
