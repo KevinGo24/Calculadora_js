853f75c18c5c670541d3f04cf4bd58175cde1d19
Frank Junior <05frankiglesiaswitt@gmail.com>
2026-02-12 18:35:11 -0500

    agregar todo

--- Logic/Calculadora.js
+++ Logic/Calculadora.js
@@ -2,21 +2,37 @@ const Numero_1 = document.getElementById("Numero_A");
 const Numero_2 = document.getElementById("Numero_b");
 const resultado = document.getElementById("Mostrar");
 function Resta() {
+     if (Numero_1.value == "") {
+        alert("Por favor digite un nombre ")
+        return;
+    }
     let a = parseFloat(Numero_1.value)
     let b = parseFloat(Numero_2.value)
     resultado.innerText = a - b
 }
 function Sumar() {
+     if (Numero_1.value == "") {
+        alert("Por favor digite su Nombre")
+        return;
+    }
     let a = parseFloat(Numero_1.value)
     let b = parseFloat(Numero_2.value)
     resultado.innerText = a + b
 }
 function Multiplicar() {
+     if (Numero_1.value == "") {
+        alert("Por favor digite su Nombre")
+        return;
+    }
     let a = parseFloat(Numero_1.value)
     let b = parseFloat(Numero_2.value)
     resultado.innerText = a * b
 }
 function Dividir() {
+     if (Numero_1.value == "") {
+        alert("Por favor digite su Nombre")
+        return;
+    }
     let a = parseFloat(Numero_1.value)
     let b = parseFloat(Numero_2.value)
     resultado.innerText = a / b
@@ -25,7 +41,7 @@ function borrar() {
     // Limpiamos los inputs
     document.getElementById('Numero_A').value = "";
     document.getElementById('Numero_b').value = "";
-    
+
     // Limpiamos el texto del resultado
     document.getElementById('resultado').innerText = "";
 }
